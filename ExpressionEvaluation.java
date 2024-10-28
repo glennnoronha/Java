2a470ddc96f29dabf716a7f3fe675ea4d7c46f50
glennnoronha <glenn.noronha@yahoo.com>
2024-10-27 22:08:09 -0500

    expression evaluation in Java vs C

---
+++ ExpressionEvaluation.java
@@ -0,0 +1,23 @@
+public class ExpressionEvaluation {
+    public static void main(String[] args) {
+        int a = 5, b = 10, c = 9, d = 6, e = 4;
+        
+        int exprA = ((a * b) - 1) + c;
+        System.out.println("Expression a: " + exprA);
+        
+        int exprB = ((a * (b - 1)) / c) % d;
+        System.out.println("Expression b: " + exprB);
+        
+        int exprC = ((a - b) / c) & ((d * e / a) - 3);
+        System.out.println("Expression c: " + exprC);
+
+        boolean exprD = (-a != 0) || ((c == d) && (e != 0));
+        System.out.println("Expression d: " + exprD);
+        
+        boolean exprE = ((a > b) ^ (c != 0)) || (d <= 17);
+        System.out.println("Expression e: " + exprE);
+        
+        int exprF = (-a) + b;
+        System.out.println("Expression f: " + exprF);
+    }
+}
